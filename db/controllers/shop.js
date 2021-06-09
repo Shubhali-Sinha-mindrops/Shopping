59264604dafad7eab7bda73d1361923fba796a6f
Shubhali Sinha <shubhali@mindrops.com>
2021-06-09 17:26:36 +0530

    Fourth commit

--- db/controllers/shop.js
+++ db/controllers/shop.js
@@ -1,8 +1,8 @@
 const User = require('../models/user');
 const Carts = require('../models/carts');
-const Sequelize = require('sequelize');
 
-module.exports = {
+
+ module.exports = {
     list(req,res) {
         return User
         .findAll({
@@ -23,13 +23,13 @@ module.exports = {
                 model: Carts,
             }],
         })
-        .then((user) => {
-            if(!user) {
+        .then((users) => {
+            if(!users) {
                 return res.status(404).send({
                     message: 'User Not Found',
                 });
             }
-            return res.status(200).send(user);
+            return res.status(200).send(users);
         })
         .catch((error) => res.status(400).send(error));
     },
@@ -38,7 +38,7 @@ module.exports = {
         .create({
             class_name: req.body.class_name,
         })
-        .then((user) => res.status(201).send(user))
+        .then((users) => res.status(201).send(users))
         .catch((error) => res.status(400).send(error));
     },
 };
@@ -51,40 +51,3 @@ module.exports = {
 
 
 
-
-/*
-const postOrder = (req,res, next) => {
-    User.find({select: ['id']})
-    .then(userId => {
-        const userID = userId[userId.length - 1];
-        const order = new Order();
-        order.userid = userID;
-        order.save();
-        console.log('Hello');
-            setTimeout(() => {
-                Order.find({relations: ['userid'], where: {userid: userID}, order: {id: 'DESC'}, take:1})
-                .then(ord => {
-                 CartProduct.find({relations: ['cartid', 'productId'], where: {cartid: userID}})
-                .then(cItem => {
-                    cItem.forEach(oItem => {
-                        const orderProduct = new OrderProduct();
-                        orderProduct.orderId = ord[0];
-                        orderProduct.productId = oItem.productId;
-                        orderProduct.save();
-                        CartProduct.delete({cartid: userID});
-                    });
-                    setTimeout(() => {
-                        res.redirect('/orders');
-                    }, 300);
-                })
-                .catch(console.log);
-            })
-            .catch(console.log);
-        }, 700);
-    })
-    .catch(console.log);
-};
-
-module.exports = {
-    postOrder,
-};*/

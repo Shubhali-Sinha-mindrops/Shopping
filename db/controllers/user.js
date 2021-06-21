9da58c8579ae126c6e34964d0bd813da5ae467f4
Shubhali-Sinha-mindrops <84384541+Shubhali-Sinha-mindrops@users.noreply.github.com>
2021-06-21 10:29:10 +0530

    Update user.js

--- db/controllers/user.js
+++ db/controllers/user.js
@@ -82,7 +82,7 @@ exports.signIn = async(req,res) => {
     }
 
     var token = jwt.sign({ id: user.id},config.secret,{
-      expiresIn: 86400 //24 hrs
+      expiresIn: 1545890
     });
 
     var authorities = [];
